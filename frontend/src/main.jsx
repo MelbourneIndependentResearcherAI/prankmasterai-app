f44eb6d8d4393cd2ba43c30a5d1989f057695e10
MelbourneIndependentResearcherAI <bruzii86@gmail.com>
2026-01-21 21:45:02 +1100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Chat from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Chat />
-  </React.StrictMode>
-);
