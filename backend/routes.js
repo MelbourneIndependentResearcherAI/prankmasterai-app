e9f491e3315e926d1545d62916f012b413b0da97
MelbourneIndependentResearcherAI <bruzii86@gmail.com>
2026-01-25 23:59:04 +1100

    Delete backend/routes.js

--- backend/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handleMessage } from "../controllers/messageController.js";
-
-const router = express.Router();
-
-router.post("/", handleMessage);
-
-export default router;
