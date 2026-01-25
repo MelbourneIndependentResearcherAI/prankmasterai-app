84517a472b8e785994958cd419807d4c31eb3023
MelbourneIndependentResearcherAI <bruzii86@gmail.com>
2026-01-25 17:44:50 +1100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ padding: "20px", fontSize: "20px" }}>
-      <p>Temporary app shell.</p>
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
